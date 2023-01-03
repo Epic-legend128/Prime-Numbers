89eb50020a800ed65bac5a9636876125a53d2d7a
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-03 22:18:25 +0100

    Create BruteForce.cpp

---
+++ C++/BruteForce.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//takes in a number and returns true if it is prime and false otherwise, worst possible solution with O(N) time notation
+bool isPrime(int num) {
+    if (num == 2) return true;
+    for (int i = 2; i<num; i++) {
+        if (!(num%i)) return false;
+    }
+    return true;
+}
+
+
+int main() {
+    int num;
+    cout << "Enter a number to check if it's prime or not.\n";
+    cin >> num;
+    cout << "The number you entered is "<<(isPrime(num) ? "prime" : "not prime")<< "\n";
+    return 0;
+}
