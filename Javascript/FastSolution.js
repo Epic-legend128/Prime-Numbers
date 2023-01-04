8cc9993f50cd97a1cd2cdde993b03bdec38804b6
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 13:44:44 +0100

    Create FastSolution.js

---
+++ Javascript/FastSolution.js
@@ -0,0 +1,11 @@
+//relatively fast solution, O(n^(1/2)) time notation
+function primalityTest(num) {
+    if (num%2==0) return num==2;
+    for (let i = 3; i*i<=num; i+=2) {
+        if (num%i==0) return false;
+    }
+    return true;
+}
+
+const test = 10; // number to check
+console.log("The number "+test+" is "+(primalityTest(test)?"prime":"not prime"));
