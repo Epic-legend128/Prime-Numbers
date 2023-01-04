258edeb6607e6201a2ebb20cab79ef9f68224b33
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 20:02:22 +0100

    Special case fix

--- Javascript/BruteForce.js
+++ Javascript/BruteForce.js
@@ -1,5 +1,6 @@
 //brute force, O(n) time notation
 function isPrime(num) {
+    if (num <= 1) return false;
     if (num==2) return true;
     for (let i = 2; i<num; i++) {
         if (num%i==0) return false;
