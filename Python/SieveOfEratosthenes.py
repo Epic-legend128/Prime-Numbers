7dd900f522c5edd3a1649004267789660d502c5b
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 20:25:55 +0100

    Create SieveOfEratosthenes.py

---
+++ Python/SieveOfEratosthenes.py
@@ -0,0 +1,22 @@
+def sieve_eratosthenes(lower, upper):
+    primes = []
+    is_prime = []
+    for i in range(0, upper+1):
+        is_prime.append(True)
+    is_prime[0], is_prime[1] = False, False
+
+    for i in range(2, upper+1):
+        if is_prime[i]:
+            for j in range(i*i, upper+1, i):
+                is_prime[j] = False
+            if i >= lower:
+                primes.append(i)
+    return primes
+
+
+print("This method finds all prime numbers between a range")
+lower = int(input("Enter the lower bound of the range: "))
+upper = int(input("Enter the upper bound of the range: "))
+primes = sieve_eratosthenes(lower, upper)
+print("A list of all of the primes is the following:")
+print(", ".join(map(str, primes)))
