a62e3168b188c47cffe8bf2df3eff0cef24c93ff
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 20:04:00 +0100

    Special case fix

--- Javascript/FastSolution.js
+++ Javascript/FastSolution.js
@@ -1,5 +1,6 @@
 //relatively fast solution, O(n^(1/2)) time notation
 function primalityTest(num) {
+    if (num <= 1) return false;
     if (num%2==0) return num==2;
     for (let i = 3; i*i<=num; i+=2) {
         if (num%i==0) return false;
