42d70a57aba7ee469f4f443c858fdab8e27cd685
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 13:37:41 +0100

    Create BruteForce.js

---
+++ Javascript/BruteForce.js
@@ -0,0 +1,11 @@
+//brute force, O(n) time notation
+function isPrime(num) {
+    if (num==2) return true;
+    for (let i = 2; i<num; i++) {
+        if (num%i==0) return false;
+    }
+    return true;
+}
+
+const test = 10; // number to check
+console.log("The number "+test+" is "+(isPrime(test)?"prime":"not prime"));
