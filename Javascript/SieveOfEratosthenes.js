77a4b5096121ca8df52cdef9638af121f9acd74e
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-04 17:36:53 +0100

    Create SieveOfEratosthenes.js

---
+++ Javascript/SieveOfEratosthenes.js
@@ -0,0 +1,21 @@
+function sieveOfEratosthenes(lower, upper) {
+    var isPrime = [];
+    for (let i = 0; i<=upper; i++) isPrime.push(true);
+    isPrime[0] = false;
+    isPrime[1] = false;
+    var primes = [];
+    for(let i = 2; i<=upper; i++) {
+        if (isPrime[i]) {
+            primes.push(i);
+            for (let j = i*2; j<=upper; j+=i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    return primes;
+}
+
+const lower = 0;
+const upper = 10;
+const primes = sieveOfEratosthenes(lower, upper);
+console.log(primes.join(", "));
