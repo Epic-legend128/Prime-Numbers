08280206bc4aef4c3c707775c52b6fa9e7c21330
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-05 18:51:39 +0100

    For bigger numbers

--- C++/LittleTheoremOfFermat.cpp
+++ C++/LittleTheoremOfFermat.cpp
@@ -3,15 +3,15 @@
 #include <random>
 
 //gcd
-int gcd(int a, int b) {
+long long gcd(long long a, long long b) {
     if (a<b) std::swap(a, b);
     if (!(a%b)) return b;
     return gcd(b, a%b);
 }
 
 //power, (base^exp)%n
-int power(int base, int exp, int n) {
-    int result = 1;
+long long power(long long base, long long exp, long long n) {
+    long long result = 1;
     base %= n;
 
     while(exp > 0) {
@@ -23,13 +23,13 @@ int power(int base, int exp, int n) {
 }
 
 //a very fast way to check if a number is prime but it doesn't have a 100% propability of success, works best with not very big inputs, O(klogn) time notation
-bool little_theorem_of_fermat(const int num) {
+bool little_theorem_of_fermat(const long long num) {
     if (num==3||num==2) return true;
     else if (num<=1 || num%2==0) return false;
 
-    const int checks = std::min(20, num);
+    const int checks = num < 20 ? num : 20;
     for (int i = 0; i<checks; i++) {
-        int current = rand()%(num-4)+2;
+        long long current = rand()%(num-4)+2;
         if (gcd(num, current) != 1) return false;
         if (power(current, num-1, num) != 1) return false;
     }
@@ -38,7 +38,7 @@ bool little_theorem_of_fermat(const int num) {
 }
 
 int main() {
-    int num;
+    long long num;
     std::cout << "Enter a number to check if it's prime or not.\n";
     std::cin >> num;
     std::cout << "The number you entered is "<<(little_theorem_of_fermat(num) ? "prime" : "not prime")<< "\n";
