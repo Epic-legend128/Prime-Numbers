6fbbc3d5e07bf8c09e069c0fc0db6b83d8e99ef1
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-14 20:17:27 +0100

    Create LittleTheoremOfFermat.c

---
+++ C/LittleTheoremOfFermat.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+//gcd
+long long gcd(long long a, long long b) {
+    if (a<b) gcd(b, a);
+    if (!(a%b)) return b;
+    return gcd(b, a%b);
+}
+
+//power, (base^exp)%n
+long long power(long long base, long long exp, long long n) {
+    long long result = 1;
+    base %= n;
+
+    while(exp > 0) {
+        if(exp & 1) result=(result*base)%n;
+        exp>>=1;
+        base=(base*base)%n;
+    }
+    return result;
+}
+
+//a very fast way to check if a number is prime but it doesn't have a 100% propability of success, works best with not very big inputs, O(klogn) time notation
+bool little_theorem_of_fermat(const long long num) {
+    if (num==3||num==2) return true;
+    else if (num<=1 || num%2==0) return false;
+
+    const int checks = num < 20 ? num : 20;
+    for (int i = 0; i<checks; i++) {
+        long long current = rand()%(num-4)+2;
+        if (gcd(num, current) != 1) return false;
+        if (power(current, num-1, num) != 1) return false;
+    }
+
+    return true;
+}
+
+int main() {
+    long long num;
+    printf("Enter a number to check if it's prime or not.\n");
+    scanf("%lli", &num);
+    if (little_theorem_of_fermat(num)) printf("The number you entered is prime\n");
+    else printf("The number you entered is not prime\n");
+    return 0;
+}
