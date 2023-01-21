32f16e34736f45a0997cbee077678b225f63030f
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-01-21 18:42:24 +0100

    Create SieveOfEratosthenes.php

---
+++ PHP/SieveOfEratosthenes.php
@@ -0,0 +1,31 @@
+<?php
+function sieveOfEratosthenes($lower, $upper) {
+    $is_prime = array_fill(0, $upper+1, true);
+    $is_prime[0] = false;
+    $is_prime[1] = false;
+    $primes = [];
+    for ($i = 2; $i <= $upper; $i++) {
+        if ($is_prime[$i] == true) {
+            for ($j = $i*2; $j<=$upper; $j+=$i) {
+                $is_prime[$j] = false;
+            }
+            if ($i>=$lower) array_push($primes, $i);
+        }
+    }
+    return $primes;
+}
+
+$lower = 1;
+$upper = 10;
+$primes = sieveOfEratosthenes($lower, $upper);
+if (count($primes) > 0) {
+    echo $primes[0];
+    for ($i = 1; $i<count($primes); $i++) {
+        echo ", ";
+        echo $primes[$i];
+    }
+}
+else {
+    echo "There are no prime numbers within the given range";
+}
+?>
