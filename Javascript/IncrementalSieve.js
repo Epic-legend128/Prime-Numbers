eb19ce5eba5e2091ad564e5566b95932515431c0
Fotios Vaitsopoulos <100092577+Epic-legend128@users.noreply.github.com>
2023-08-10 18:46:49 +0300

    Update IncrementalSieve.js

--- Javascript/IncrementalSieve.js
+++ Javascript/IncrementalSieve.js
@@ -1,20 +1,28 @@
 //incremental sieve
 function incrementalSieve(upper) {
-  let primes = [2, 3];
+  let original = [2, 3];
+  let primes = [[3, 3]];
   let isPrime;
 
   for (let i = 5; i<upper; i+=2) {
     isPrime = true;
-    for (let j = 0; j<primes.length-1; j++) {
-      if (i%primes[j] == 0) {
+    let right = Math.sqrt(i);
+    for (let j = 0; j<primes.length; j++) {
+      if (primes[j][0] > right) break;
+      while (primes[j][1] < i) {
+        primes[j][1] += primes[j][0];
+      }
+      if (primes[j][1] == i) {
         isPrime = false;
         break;
       }
     }
+
     if (isPrime) {
-      primes.push(i);
+      primes.push([i, i]);
+      original.push(i);
     }
+    
   }
-  
-  return primes;
+  return original;
 }
